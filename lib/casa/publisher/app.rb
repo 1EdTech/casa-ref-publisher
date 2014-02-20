ac604a2c169a38bd82dae1c536c6e407750844f3
Eric Bollens <ebollens@ucla.edu>
2014-02-20 15:34:02 -0800

    Break Sinatra functionality out into strategy object

--- lib/casa/publisher/app.rb
+++ lib/casa/publisher/app.rb
@@ -1,53 +1,53 @@
 require 'sinatra/base'
-require 'sinatra/json'
 require 'casa/publisher/persistence/memory_storage_handler'
+require 'casa/publisher/strategy/sinatra'
 
 module CASA
   module Publisher
     class App < Sinatra::Base
 
       @@storage_handler = false
+
       @@postprocess_handler = false
 
       def self.set_postprocess_handler handler
+
         @@postprocess_handler = handler
+
       end
 
       def self.set_storage_handler handler
+
         @@storage_handler = handler
+
       end
 
       def self.postprocess_handler
+
         @@postprocess_handler
+
       end
 
       def self.storage_handler
+
         @@storage_handler
+
       end
 
       get '/out/payloads' do
 
-        begin
-          request.accept? 'application/json'
-        rescue
-          error 406, "Not Acceptable"
-        end
+        handler = CASA::Publisher::Strategy::Sinatra.new self,
+          'from_handler' => @@storage_handler,
+          'postprocess_handler' => @@postprocess_handler
 
-        error 501, "Not Implemented" unless @@storage_handler
-
-        # NOTE: error 415 should be thrown if client processes body and unsupported request Content-Type
-        # NOTE: error 400 should be thrown if client processes body and it is malformed per the Content-Type
-
-        json @@storage_handler.get_all.map(){ |payload|
-          @@postprocess_handler ? @@postprocess_handler.execute(payload) : payload
-        }.select(){ |payload|
-          payload
-        }
+        handler.execute
 
       end
 
       get '/payloads' do
+
         call! env.merge("PATH_INFO" => '/out/payloads')
+
       end
 
     end
